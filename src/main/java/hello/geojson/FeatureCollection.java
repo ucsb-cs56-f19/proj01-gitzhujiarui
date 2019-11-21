10d8c921b8307876aec3652d7ea55db7c1148f77
Jiarui <jiarui_zhu@ucsb.edu>
2019-11-20 16:41:12 -0800

    jz - use objects to display results

--- src/main/java/hello/geojson/FeatureCollection.java
+++ src/main/java/hello/geojson/FeatureCollection.java
@@ -1,16 +1,19 @@
+package hello.geojson;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.core.JsonProcessingException;
+import java.util.List;
 
-package hello.geojson;
 
 public class FeatureCollection {
   private static Logger logger = LoggerFactory.getLogger(FeatureCollection.class);
   public String type;
   public Metadata metadata;
+  public List<Feature> features;
 
 
 
